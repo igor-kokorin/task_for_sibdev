1301ae565c226a0a3eaf196be6ce640325e7ff87
Igor Kokorin <kokorin.fear333777@yandex.ru>
2018-09-26 19:59:36 +0700

    Replace ejs with nunjucks

--- web/register-views.ts
+++ web/register-views.ts
@@ -1,15 +1,33 @@
 import { Server } from 'hapi';
 import Vision = require('vision');
-import ejs = require('ejs');
+import nunjucks = require('nunjucks');
+import path = require('path');
 import Promise = require('bluebird');
 
 export class RegisterViews {
     public static register(options: { server: Server }): Promise<any> {
         return Promise.cast(options.server.register(Vision).then(() => {
             options.server.views({
-                engines: { ejs },
-                relativeTo: __dirname,
-                path: 'views'
+                engines: {
+                    html: {
+                        compile: function (src, options) {
+                            const template = nunjucks.compile(src, options.environment);
+        
+                            return (context) => {
+        
+                                return template.render(context);
+                            };
+                        },
+        
+                        prepare: function (options, next) {
+                            (<any>options).compileOptions.environment = nunjucks.configure((<any>options).path, { watch : false });
+
+                            return next();
+                        }
+                    }
+                },
+                relativeTo: path.join(__dirname, 'views'),
+                path: path.join(process.cwd(), 'prod', 'web', 'views')
             });
         }));
     }
